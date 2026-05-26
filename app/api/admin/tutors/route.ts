67afbb54e21cad96aef60f6cb2a7c44ca9c453aa
Francis David Okon <francisdavidokon77@gmail.com>
2026-05-26 18:19:25 +0100

    Created the enpoint to fetch tutors' data

--- app/api/admin/tutors/route.ts
+++ app/api/admin/tutors/route.ts
@@ -9,13 +9,45 @@ export async function GET(request: NextRequest) {
       },
 
       select: {
+        id: true,
         fullName: true,
         email: true,
-        courses: true,
+        courses: {
+          select: {
+            id: true,
+            name: true,
+
+            students: {
+              select: {
+                studentId: true,
+              },
+            },
+          },
+        },
       },
     });
 
-    return NextResponse.json({ success: true, tutors });
+    const tutorData = tutors.map((tutor) => {
+      // Get students' ID
+      const studentIds = tutor.courses.flatMap((course) =>
+        course.students.map((student) => student.studentId),
+      );
+
+      // Remove duplicates
+      const uniqueStudents = [...new Set(studentIds)];
+
+      const assignedCourses = tutor.courses.map((course) => course.name);
+
+      return {
+        id: tutor.id,
+        fullName: tutor.fullName,
+        email: tutor.email,
+        assignedCourses,
+        totalStudents: uniqueStudents.length,
+      };
+    });
+
+    return NextResponse.json({ success: true, tutorData });
   } catch (error: any) {
     return NextResponse.json({ error: error.message });
   }
