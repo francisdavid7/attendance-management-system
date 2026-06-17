3420e9f990b8771f4e94656920f5657a2ab1d67b
Francis David Okon <francisdavidokon77@gmail.com>
2026-06-17 08:32:35 +0100

    Fixed a component

--- components/landing/marquee.tsx
+++ components/landing/marquee.tsx
@@ -1,6 +1,6 @@
 const marquee = () => {
   return (
-    <div className="w-full overflow-hidden mx-auto max-w-[80%] relative mt-10 before:z-50 before:w-[30%] before:absolute before:h-full before:block before:top-0 before:bg-linear-to-r before:from-(--color-card) before:to-card/30 before:left-0  after:w-[30%] after:absolute after:h-full after:block after:top-0 after:bg-linear-to-l after:from-(--color-card) after:to-card/30 after:right-0">
+    <div className="w-full overflow-hidden mx-auto md:max-w-[80%] w-full relative mt-10 before:z-50 before:w-[30%] before:absolute before:h-full before:block before:top-0 before:bg-linear-to-r before:from-(--color-card) before:to-card/30 before:left-0  after:w-[30%] after:absolute after:h-full after:block after:top-0 after:bg-linear-to-l after:from-(--color-card) after:to-card/30 after:right-0">
       <div className="flex w-max animate-marquee mt-5">
         <div className="flex shrink-0 whitespace-nowrap">
           <span className="text-5xl mx-8 ">
